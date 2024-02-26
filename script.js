bb017f9da4a2b98744a29db65ad779e3d9ac388e
Kvel12 <kevin.alejandro.velez@correounivalle.edu.co>
2024-02-26 17:03:02 -0500

    Agregación de un botón con función de Cammbio de Color
    Se agregó un botón en el archivo HTML, y el archivo JavaScript se editó para que cambie el color del botón agregado

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 document.getElementById('colorButton').addEventListener('click', function() {
-    document.body.style.backgroundColor = getRandomColor();
-  });
-  
-  function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  document.body.style.backgroundColor = getRandomColor();
+});
+
+document.getElementById('cambiarColor').addEventListener('click', function() {
+  this.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
-  
+  return color;
+}
+
